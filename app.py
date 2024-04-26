a9ad5b4b593bedb0f2351d7bf9d6ecd0b23d72d6
SilentAssassin101 <treytonwilkerson@gmail.com>
2024-04-26 03:27:54 -0400

    fixed to flake8 standards

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ class Loan(object):
             return self.remaining
         self.remaining -= amount
         if self.remaining == 0:
-            #ISSUE: Causes errors with UI
+            # ISSUE: Causes errors with UI
             print('Loan paid off')
             self.remove_loan()
         else:
@@ -122,7 +122,7 @@ def runTests():
     exit()
 
 
-def create_loan(id, memo, amount, term, save = False):
+def create_loan(id, memo, amount, term, save=False):
     # check if already exists
     try:
         get_loan(id)
@@ -144,7 +144,14 @@ def create_loan(id, memo, amount, term, save = False):
 
 def get_loan_obj(id):
     loanDict = get_loan(id)
-    return Loan(loanDict['id'], loanDict['memo'], loanDict['amount'], loanDict['term'], loanDict['remaining'], True)
+    return Loan(
+        loanDict['id'],
+        loanDict['memo'],
+        loanDict['amount'],
+        loanDict['term'],
+        loanDict['remaining'],
+        True
+    )
 
 
 def generate_id():
@@ -177,28 +184,63 @@ class LoanAppGUI(tk.CTk):
         frame = tk.CTkFrame(master=root)
         frame.pack(pady=20, padx=60, fill="both", expand=True)
 
-        mainLabel = tk.CTkLabel(master=frame, text='Loan App', font=('Roboto', 24))
+        mainLabel = tk.CTkLabel(
+            master=frame,
+            text='Loan App',
+            font=('Roboto', 24)
+        )
         mainLabel.pack(pady=12, padx=10)
 
-        self.loanListbox = otk.Listbox(master=frame, font=('Roboto', 12), bg='#3c3c3c', width=100, height=10, bd=0, highlightthickness=0, fg='white')
+        self.loanListbox = otk.Listbox(
+            master=frame,
+            font=('Roboto', 12),
+            bg='#3c3c3c',
+            width=100,
+            height=10,
+            bd=0,
+            highlightthickness=0,
+            fg='white'
+        )
         self.loanListbox.pack(pady=12, padx=10)
 
         self.loanInfoFrame = tk.CTkFrame(master=frame)
         self.loanInfoFrame.pack(pady=12, padx=10)
 
-        self.loanInfoLabel = tk.CTkLabel(master=self.loanInfoFrame, text='Loan Info', font=('Roboto', 16))
+        self.loanInfoLabel = tk.CTkLabel(
+            master=self.loanInfoFrame,
+            text='Loan Info',
+            font=('Roboto', 16)
+        )
         self.loanInfoLabel.pack(pady=12, padx=10)
 
-        self.loanInfoText = tk.CTkTextbox(master=self.loanInfoFrame, font=('Roboto', 16), width=250)
+        self.loanInfoText = tk.CTkTextbox(
+            master=self.loanInfoFrame,
+            font=('Roboto', 16),
+            width=250
+        )
         self.loanInfoText.pack(pady=12, padx=10)
 
-        self.makePaymentButton = tk.CTkButton(master=self.loanInfoFrame, text='Make Payment', font=('Roboto', 16), command=self.makePayment)
+        self.makePaymentButton = tk.CTkButton(
+            master=self.loanInfoFrame,
+            text='Make Payment',
+            font=('Roboto', 16),
+            command=self.makePayment
+        )
         self.makePaymentButton.pack(pady=12, padx=10)
 
-        self.addLoanButton = tk.CTkButton(master=frame, text='Add Loan', font=('Roboto', 16), command=self.openAddLoanWindow)
+        self.addLoanButton = tk.CTkButton(
+            master=frame, text='Add Loan',
+            font=('Roboto', 16),
+            command=self.openAddLoanWindow
+        )
         self.addLoanButton.pack(pady=12, padx=10)
 
-        self.exitButton = tk.CTkButton(master=frame, text='Exit', font=('Roboto', 16), hover_color='red', command=root.quit)
+        self.exitButton = tk.CTkButton(
+            master=frame, text='Exit',
+            font=('Roboto', 16),
+            hover_color='red',
+            command=root.quit
+        )
         self.exitButton.pack(pady=12, padx=10)
 
         loans = get_loans()
@@ -221,8 +263,14 @@ class LoanAppGUI(tk.CTk):
         self.loanInfoText.insert('end', 'Memo: ' + loan['memo'] + '\n')
         self.loanInfoText.insert('end', f"Amount: ${loan['amount']:,.2f}\n")
         self.loanInfoText.insert('end', 'Term: ' + str(loan['term']) + '\n')
-        self.loanInfoText.insert('end', f"Monthly Payment: ${loan['monthlyPayment']:,.2f}\n")
-        self.loanInfoText.insert('end', f"Remaining: ${loan['remaining']:,.2f}")
+        self.loanInfoText.insert(
+            'end',
+            f"Monthly Payment: ${loan['monthlyPayment']:,.2f}\n"
+        )
+        self.loanInfoText.insert(
+            'end',
+            f"Remaining: ${loan['remaining']:,.2f}"
+        )
 
     def get_selected_data(self):
         selection = self.loanListbox.curselection()
@@ -232,7 +280,8 @@ class LoanAppGUI(tk.CTk):
             return data
 
     def addLoan(self, id, memo):
-        """This method is meant to load an already created loan to the UI since there is no way to handle state in the UI
+        """This method is meant to load an already created loan to the UI
+        since there is no way to handle state in the UI
 
         Args:
             id (int): id of the loan
@@ -245,7 +294,11 @@ class LoanAppGUI(tk.CTk):
         LoanAppAddLoanGUI(self)
 
     def makePayment(self):
-        dialog = tk.CTkInputDialog(text='Enter payment amount', title='Make Payment', font=('Roboto', 16))
+        dialog = tk.CTkInputDialog(
+            text='Enter payment amount',
+            title='Make Payment',
+            font=('Roboto', 16)
+        )
         amount = dialog.get_input()
         if not amount:
             return
@@ -254,7 +307,7 @@ class LoanAppGUI(tk.CTk):
         loan = get_loan_obj(id)
         if loan.make_payment(float(amount)) == 0:
             self.loanListbox.delete(self.loanListbox.curselection())
-            #clear loan info
+            # clear loan info
             self.loanInfoText.delete('1.0', 'end')
         else:
             self.show_loan_info(None)
@@ -275,35 +328,57 @@ class LoanAppAddLoanGUI(tk.CTkToplevel):
         frame = tk.CTkFrame(master=self)
         frame.pack(pady=20, padx=60, fill="both", expand=True)
 
-        mainLabel = tk.CTkLabel(master=frame, text='Add Loan', font=('Roboto', 24))
+        mainLabel = tk.CTkLabel(
+            master=frame,
+            text='Add Loan',
+            font=('Roboto', 24)
+        )
         mainLabel.pack(pady=12, padx=10)
 
-        memoLabel = tk.CTkLabel(master=frame, text='Memo:', font=('Roboto', 16))
+        memoLabel = tk.CTkLabel(
+            master=frame,
+            text='Memo:',
+            font=('Roboto', 16)
+        )
         memoLabel.pack(pady=12, padx=10)
 
         self.memoEntry = tk.CTkEntry(master=frame, font=('Roboto', 16))
         self.memoEntry.pack(pady=12, padx=10)
 
-        amountLabel = tk.CTkLabel(master=frame, text='Amount:', font=('Roboto', 16))
+        amountLabel = tk.CTkLabel(
+            master=frame,
+            text='Amount:',
+            font=('Roboto', 16)
+        )
         amountLabel.pack(pady=12, padx=10)
 
         self.amountEntry = tk.CTkEntry(master=frame, font=('Roboto', 16))
         self.amountEntry.pack(pady=12, padx=10)
 
-        termLabel = tk.CTkLabel(master=frame, text='Term:', font=('Roboto', 16))
+        termLabel = tk.CTkLabel(
+            master=frame,
+            text='Term:',
+            font=('Roboto', 16)
+        )
         termLabel.pack(pady=12, padx=10)
 
         self.termEntry = tk.CTkEntry(master=frame, font=('Roboto', 16))
         self.termEntry.pack(pady=12, padx=10)
 
-        saveButton = tk.CTkButton(master=frame, text='Save', font=('Roboto', 16), command=self.save_loan)
+        saveButton = tk.CTkButton(
+            master=frame, text='Save', font=('Roboto', 16),
+            command=self.save_loan
+        )
         saveButton.pack(pady=12, padx=10)
 
-        cancelButton = tk.CTkButton(master=frame, text='Cancel', font=('Roboto', 16), command=self.destroy)
+        cancelButton = tk.CTkButton(
+            master=frame, text='Cancel', font=('Roboto', 16),
+            command=self.destroy
+        )
         cancelButton.pack(pady=12, padx=10)
 
     def save_loan(self):
-        id = 0 #id will be generated later
+        id = 0  # id will be generated later
         memo = self.memoEntry.get()
         amount = float(self.amountEntry.get())
         term = int(self.termEntry.get())
